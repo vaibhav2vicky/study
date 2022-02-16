eaa0415b21bcf26467e8d336ccbaf7fe5a401dc6
Vaibhav Kumar Yadav <49692739+vaibhav2vicky@users.noreply.github.com>
2022-02-16 04:00:57 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
+# uncomment while usng in kali 
+# echo "deb http://kali.cs.nctu.edu.tw/kali kali-rolling main non-free contrib" | sudo tee /etc/apt/sources.list
+
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 
-apt update && apt upgrade -y && apt install terminator gobuster feroxbuster joplin python3-pip neofetch lolcat apt-transport-https sublime-text seclists -y
+apt update && apt upgrade -y && apt install terminator gobuster feroxbuster joplin python3-pip neofetch lolcat apt-transport-https sublime-text seclists socat gdebi-core -y
 
 gem install evil-winrm
 
 cd /opt
+
+sudo wget https://github.com/shiftkey/desktop/releases/download/release-2.9.3-linux3/GitHubDesktop-linux-2.9.3-linux3.deb
 
 git clone https://github.com/swisskyrepo/PayloadsAllTheThings.git
 
@@ -18,12 +23,13 @@ git clone https://github.com/carlospolop/PEASS-ng.git
 
 git clone https://github.com/SecureAuthCorp/impacket.git
 
+sudo gdebi GitHubDesktop-linux-2.9.3-linux3.deb -y
+
 cd impacket 
 
 python3 setup.py install
 
 cd ~
 
-
-
+# Uncomment while using first time
 # reboot
