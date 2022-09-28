fd6aefb3c347fa8c79a751f2bf5e6eb1e700c53b
Vaibhav Kumar Yadav <49692739+vaibhav2vicky@users.noreply.github.com>
2022-09-28 22:30:09 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-# uncomment while using in kali for faster downloads
-# echo "deb http://kali.cs.nctu.edu.tw/kali kali-rolling main non-free contrib" | sudo tee /etc/apt/sources.list
+apt update && apt upgrade -y && apt install gobuster feroxbuster python3-pip neofetch lolcat apt-transport-https bloodhound seclists socat kali-wallpapers-all kali-linux-large -y
 
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg
-
-echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-
-apt update && apt upgrade -y && apt install gobuster feroxbuster joplin python3-pip neofetch lolcat apt-transport-https sublime-text seclists socat kali-wallpapers-all kali-linux-large -y
+pip3 install pwntools
 
 gem install evil-winrm
 
 cd /opt
+
+git clone https://github.com/ParrotSec/mimikatz.git
+
+git clone https://github.com/BloodHoundAD/BloodHound.git
 
 git clone https://github.com/swisskyrepo/PayloadsAllTheThings.git
 
@@ -19,13 +18,8 @@ git clone https://github.com/rebootuser/LinEnum.git
 
 git clone https://github.com/carlospolop/PEASS-ng.git
 
-git clone https://github.com/SecureAuthCorp/impacket.git
-
-cd impacket 
-
-python3 setup.py install
-
 cd ~
 
 # Uncomment while using first time
+
 # reboot
