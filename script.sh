66fe3e81c09bcb4db2a66391ba73b0aa2e301d7b
Vaibhav Kumar Yadav <49692739+vaibhav2vicky@users.noreply.github.com>
2022-10-30 12:00:43 -0400

    ini

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt update && sudo apt upgrade -y && sudo apt install gobuster feroxbuster python3-pip neofetch lolcat apt-transport-https bloodhound seclists socat kali-wallpapers-all kali-linux-large docker.io -y
+sudo apt update && sudo apt upgrade -y && sudo apt install gobuster feroxbuster python3-pip neofetch lolcat apt-transport-https bloodhound seclists socat kali-wallpapers-all bat -y
 
 pip3 install pwntools
 
@@ -21,6 +21,8 @@ git clone https://github.com/swisskyrepo/PayloadsAllTheThings.git
 git clone https://github.com/rebootuser/LinEnum.git
 
 git clone https://github.com/carlospolop/PEASS-ng.git
+
+git clone https://github.com/r3motecontrol/Ghostpack-CompiledBinaries.git
 
 cd ~
 
