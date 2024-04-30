2e9c5a093e1de4cd3fa27b3095aa306300d4eb00
Vaibhav Kumar Yadav <49692739+vaibhav2vicky@users.noreply.github.com>
2024-05-01 01:09:11 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-sudo apt update && sudo apt upgrade -y && sudo apt install gobuster feroxbuster python3-pip neofetch lolcat apt-transport-https bloodhound seclists socat kali-wallpapers-all bat -y
+sudo apt update && sudo apt upgrade -y && sudo apt install gobuster feroxbuster pipx neofetch lolcat apt-transport-https bloodhound seclists socat kali-wallpapers-all bat golang-go -y
 
 sudo gem install evil-winrm
+
+pipx install bhedak
 
 sudo chown -R $USER:$USER /opt
 
@@ -24,9 +26,50 @@ git clone https://github.com/61106960/adPEAS.git
 
 git clone https://github.com/fortra/impacket.git
 
+git clone https://github.com/eslam3kl/SQLiDetector.git
+
 cd ~
 
 git clone https://github.com/vaibhav2vicky/study.git
+
+#### go tools
+go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest\
+
+go install github.com/lc/gau/v2/cmd/gau@latest
+
+go install github.com/tomnomnom/gf@latest
+
+go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
+
+go install -v github.com/tomnomnom/anew@latest
+
+go install github.com/tomnomnom/waybackurls@latest
+
+go install github.com/tomnomnom/qsreplace@latest
+
+go install github.com/tomnomnom/httprobe@latest
+
+go install -v github.com/projectdiscovery/nuclei/v3/cmd/nuclei@latest
+
+go install -v github.com/owasp-amass/amass/v4/...@master
+
+go install github.com/tomnomnom/assetfinder@latest
+
+go install github.com/projectdiscovery/katana/cmd/katana@latest
+
+go install github.com/hahwul/dalfox/v2@latest
+
+go install github.com/ethicalhackingplayground/bxss@latest
+
+####
+cd /tmp
+git clone https://github.com/findomain/findomain.git
+cd findomain
+cargo build --release
+sudo cp target/release/findomain /usr/bin/
+findomain
+cd 
+
 
 # Uncomment while using first time
 
